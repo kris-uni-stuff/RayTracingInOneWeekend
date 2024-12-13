10e033ce04bad38f991c959d4874b227d286705f
Kristian Spoerer <pszks@nottingham.ac.uk>
2024-12-13 16:16:40 +0000

    added quads

--- RayTracingInOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend.cpp
+++ RayTracingInOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend.cpp
@@ -5,12 +5,47 @@
 #include "hittable.h"
 #include "hittable_list.h"
 #include "material.h"
+#include "quad.h"
 #include "sphere.h"
 
-
-int main() 
+void quads() 
 {
     hittable_list world;
+
+    // Materials
+    auto left_red = make_shared<lambertian>(colour(1.0, 0.2, 0.2));
+    auto back_green = make_shared<lambertian>(colour(0.2, 1.0, 0.2));
+    auto right_blue = make_shared<lambertian>(colour(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(colour(1.0, 0.5, 0.0));
+    auto lower_teal = make_shared<lambertian>(colour(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3, -2, 5), vec3(0, 0, -4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2, -2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3(3, -2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2, -3, 5), vec3(4, 0, 0), vec3(0, 0, -4), lower_teal));
+
+    camera cam;
+
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 80;
+    cam.lookfrom = point3(0, 0, 9);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
+void spheres()
+{
+        hittable_list world;
 
     auto ground_material = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
@@ -76,4 +111,9 @@ int main()
     cam.focus_dist = 10.0;
 
     cam.render(world);
+}
+
+int main()
+{
+    quads();
 }
